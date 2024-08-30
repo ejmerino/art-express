934e18039297ff365490caae9b53e88f13385093
Josué Merino Calderón <josuemerinocalderon11@gmail.com>
2024-08-29 23:38:02 -0500

    Changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,8 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-4 mt-8">
       <div className="container mx-auto text-center">
-        <p>&copy; 2024 Art Express. All rights reserved.</p>
+        <p>&copy; 2024 Art Express. Todos los Derechos Reservados.</p>
+        <p>Achig, Ferrín, Merino, Reinoso.</p>
       </div>
     </footer>
   );
